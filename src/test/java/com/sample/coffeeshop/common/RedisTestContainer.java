086a4ac59c3d9b930343e0fe37e5f7e41d2c6572
Kev Lee <seokbum.lee@bucketplace.net>
2023-12-09 13:30:41 +0900

    EmbeddedRedisConfig -> RedisTestContainer

---
+++ src/test/java/com/sample/coffeeshop/common/RedisTestContainer.java
@@ -0,0 +1,22 @@
+package com.sample.coffeeshop.common;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration
+public class RedisTestContainer {
+    private static final String REDIS_DOCKER_IMAGE = "redis";
+
+    static {
+        GenericContainer<?> REDIS_CONTAINER =
+                new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
+                        .withExposedPorts(6379)
+                        .withReuse(true);
+
+        REDIS_CONTAINER.start();
+
+        System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
+        System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+    }
+}
