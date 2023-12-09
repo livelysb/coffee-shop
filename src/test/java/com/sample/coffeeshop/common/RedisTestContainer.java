6c54172fa92504233599ab21dd725a6363580439
Kev Lee <seokbum.lee@bucketplace.net>
2023-12-09 13:57:08 +0900

    system property setting

--- src/test/java/com/sample/coffeeshop/common/RedisTestContainer.java
+++ src/test/java/com/sample/coffeeshop/common/RedisTestContainer.java
@@ -9,14 +9,10 @@ public class RedisTestContainer {
     private static final String REDIS_DOCKER_IMAGE = "redis";
 
     static {
-        GenericContainer<?> REDIS_CONTAINER =
-                new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
-                        .withExposedPorts(6379)
-                        .withReuse(true);
-
-        REDIS_CONTAINER.start();
-
-        System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
-        System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+        GenericContainer<?> redis =
+                new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE)).withExposedPorts(6379);
+        redis.start();
+        System.setProperty("spring.data.redis.host", redis.getHost());
+        System.setProperty("spring.data.redis.port", redis.getMappedPort(6379).toString());
     }
 }
